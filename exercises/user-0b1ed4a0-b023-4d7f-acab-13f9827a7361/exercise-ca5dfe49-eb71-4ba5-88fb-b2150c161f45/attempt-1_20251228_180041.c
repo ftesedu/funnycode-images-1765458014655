1e8c90be4e23d6035f32f14be38582b2ddd837a6
FTES EDUCATION <ai-learning@ftes.vn>
2025-12-28 18:00:42 +0700

    Submit exercise: user-0b1ed4a0-b023-4d7f-acab-13f9827a7361, exercise-ca5dfe49-eb71-4ba5-88fb-b2150c161f45, attempt-1

---
+++ exercises/user-0b1ed4a0-b023-4d7f-acab-13f9827a7361/exercise-ca5dfe49-eb71-4ba5-88fb-b2150c161f45/attempt-1_20251228_180041.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int sum;
+
+    // Input two integers
+    scanf("%d %d", &a, &b);
+
+    // Calculate sum
+    sum = a + b;
+
+    // Output result
+    printf("%d", sum);
+
+    return 0;
+}
