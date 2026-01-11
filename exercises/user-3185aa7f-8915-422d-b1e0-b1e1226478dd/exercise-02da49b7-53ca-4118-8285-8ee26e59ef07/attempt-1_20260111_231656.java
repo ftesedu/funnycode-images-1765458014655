ba2746f2b2dd744612f043204623f4f0da3fdac6
FTES EDUCATION <ai-learning@ftes.vn>
2026-01-11 23:16:56 +0700

    Submit exercise: user-3185aa7f-8915-422d-b1e0-b1e1226478dd, exercise-02da49b7-53ca-4118-8285-8ee26e59ef07, attempt-1

---
+++ exercises/user-3185aa7f-8915-422d-b1e0-b1e1226478dd/exercise-02da49b7-53ca-4118-8285-8ee26e59ef07/attempt-1_20260111_231656.java
@@ -0,0 +1,52 @@
+
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ControllerServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControllerServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ControllerServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String pwd = request.getParameter("pwd");
+        PrintWriter pw = response.getWriter();
+        
+        if(name.equalsIgnoreCase("Admin")&&pwd.equals("123")){
+            pw.print("Login successfully!!!");
+        }else pw.print("Login Failed!!");
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    
+}
