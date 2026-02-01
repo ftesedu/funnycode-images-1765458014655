ae38ed52ccbe421ea8721196bceaf28baa9f8dca
FTES EDUCATION <ai-learning@ftes.vn>
2026-02-01 14:11:37 +0700

    Submit exercise: user-948c36bc-c992-4583-a935-5b72ae96d68d, exercise-ac5832c2-bedc-404a-8f3a-07da4ecc0e03, attempt-1

---
+++ exercises/user-948c36bc-c992-4583-a935-5b72ae96d68d/exercise-ac5832c2-bedc-404a-8f3a-07da4ecc0e03/attempt-1_20260201_141136.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int isPalindrome(int n) {
+    int rev = 0, tmp = n;
+    while (tmp > 0) {
+        rev = rev * 10 + tmp % 10;
+        tmp /= 10;
+    }
+    return rev == n;
+}
+int hasDigit6(int n) {
+    while (n > 0) {
+        if (n % 10 == 6)
+            return 1;
+        n /= 10;
+    }
+    return 0;
+}
+int sumDigitEndsWith8(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum % 10 == 8;
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    for (int i = a; i <= b; i++) {
+        if (isPalindrome(i) && hasDigit6(i) && sumDigitEndsWith8(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
